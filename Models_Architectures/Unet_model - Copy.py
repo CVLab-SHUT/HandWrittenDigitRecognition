b0bcdf7ce83fa272327475e7dfd25a170895cf17
Reza-Tahmasbi <rezatb2002@gmail.com>
2024-03-10 22:14:27 +0330

    language_classifier_training

---
+++ Models_Architectures/Unet_model - Copy.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from keras.layers import (InputLayer, Conv2D, BatchNormalization,
+                          Dropout, MaxPooling2D, Conv2DTranspose,
+                          concatenate)
+from tensorflow.keras import Model
+from mraunet_layers import UnetEncoderBlock, UnetBottleneck, UnetDecoderBlock 
+from mramodule import MRAModule
+
+# Define the RAUnet class which inherits from the Model class in Keras
+class RAUnet(Model):
+    def __init__(self):
+        super(RAUnet, self).__init__()
+        
+        # Initialize the encoder blocks
+        self.encoder_block1 = UnetEncoderBlock(64)
+        self.encoder_block2 = UnetEncoderBlock(128)
+        self.encoder_block3 = UnetEncoderBlock(256)
+        self.encoder_block4 = UnetEncoderBlock(512)
+        
+        # Initialize the decoder blocks
+        self.decoder_block1 = UnetDecoderBlock(512)
+        self.decoder_block2 = UnetDecoderBlock(256)
+        self.decoder_block3 = UnetDecoderBlock(128)
+        self.decoder_block4 = UnetDecoderBlock(64)
+        
+        # Initialize the Multi-Resolution Attention (MRA) modules
+        self.mra_module1 = MRAModule()
+        self.mra_module2 = MRAModule()
+        self.mra_module3 = MRAModule()
+        self.mra_module4 = MRAModule()
+        
+        # Initialize the bottleneck layers
+        self.bottleneck1 = UnetBottleneck(1024)
+        self.bottleneck2 = UnetBottleneck(1024)
+        
+        # Initialize the final Convolutional layer
+        self.final_layer = Conv2D(1, (1, 1), padding = 'same', activation = 'sigmoid')
+    
+    
+    # Overriding call function
+    def call(self, inputs):
+        
+        # Encoding Architecture
+        skip_connection1, pooled_output1 = self.encoder_block1()(inputs)
+        skip_connection2, pooled_output2 = self.encoder_block2()(pooled_output1)
+        skip_connection3, pooled_output3 = self.encoder_block3()(pooled_output2)
+        skip_connection4, pooled_output4 = self.encoder_block4()(pooled_output3)
+
+        # Skip Block
+        skip_connection1 = self.mra_module1()(skip_connection1)
+        skip_connection2 = self.mra_module2()(skip_connection2)
+        skip_connection3 = self.mra_module3()(skip_connection3)
+        skip_connection4 = self.mra_module4()(skip_connection4)
+        
+        # Bottle Neck
+        bottleneck_output1 = self.bottleneck1()(pooled_output4)
+        bottleneck_output2 = self.bottleneck2()(bottleneck_output1)
+
+        # Decoding Architecture
+        decoder_output1 = self.decoder_block1()(bottleneck_output2, skip_connection1)
+        decoder_output2 = self.decoder_block2()(decoder_output1, skip_connection2)
+        decoder_output3 = self.decoder_block3()(decoder_output2, skip_connection3)
+        decoder_output4 = self.decoder_block4()(decoder_output3, skip_connection4)
+        
+        # Final output
+        final_output = self.final_layer(decoder_output4)
+        return final_output
--- Models_Architectures/language_classifier_architecture.py
+++ Models_Architectures/language_classifier_architecture.py
@@ -4,7 +4,7 @@ from mramodule import MRAModule  # Importing the MRAModule
 from keras.utils import to_categorical
 
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout, MaxPool2D
 from tensorflow.keras import Model
 
 class CustomModel(Model):
@@ -12,13 +12,18 @@ class CustomModel(Model):
         super(CustomModel, self).__init__()
 
         # Define the convolutional layer
-        self.conv_layer = Conv2D(64 , (3, 3), activation='relu', padding='same')
-
+        self.conv_layer1 = Conv2D(64 , (3, 3), activation='relu', padding='same')
+        self.conv_layer2 = Conv2D(64 , (3, 3), activation='relu', padding='same')
+        self.conv_layer3 = Conv2D(128 , (3, 3), activation='relu', padding='same')
+        self.conv_layer4 = Conv2D(128 , (3, 3), activation='relu', padding='same')
+        self.conv_layer5 = Conv2D(256 , (3, 3), activation='relu', padding='same')
+        self.conv_layer6 = Conv2D(256 , (3, 3), activation='relu', padding='same')
+        self.maxPooling = MaxPool2D(2, 2)
         # Define the dense layers
         self.dense_layer1 = Dense(512, activation = 'relu')
         self.dense_layer2 = Dense(256, activation = 'relu')
         self.dense_layer3 = Dense(128, activation = 'relu')
-        self.output_layer = Dense(10, activation = 'softmax')
+        self.output_layer = Dense(12, activation = 'softmax')
 
         # Define the flatten layer
         self.flatten_layer = Flatten()
@@ -35,11 +40,18 @@ class CustomModel(Model):
 
     def call(self, inputs):
         # Pass the inputs through the layers
-        x = self.conv_layer(inputs)
+        x = self.conv_layer1(inputs)
+        x = self.conv_layer2(x)
+        x = self.maxPooling(x)
         x = self.mra_module1(x)
+        x = self.conv_layer3(x)
+        x = self.conv_layer4(x)
+        x = self.maxPooling(x)
         x = self.mra_module2(x)
+        x = self.conv_layer5(x)
+        x = self.conv_layer6(x)
+        x = self.maxPooling(x)
         x = self.mra_module3(x)
-
         x = self.flatten_layer(x)
         x = self.dense_layer1(x)
         x = self.dropout_layer1(x)
@@ -52,7 +64,7 @@ class CustomModel(Model):
 
     def model(self):
         # Define the input shape
-        x = tf.keras.Input(shape=(64, 64, 1))
+        x = tf.keras.Input(shape=(128, 128, 1))
         return Model(inputs=[x],outputs=self.call(x))
 
 if __name__ == "__main__":
